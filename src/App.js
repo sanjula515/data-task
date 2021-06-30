216bf99d862a5931ef51510f9be901aa5ee2451a
sanjula515 <poustinas@gmail.com>
2021-06-30 10:48:24 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+ 
+
+
+class App extends React.Component{
+  render(){
+    return(
+      
+  <div>
+ <div className="App">      
+
+<div class="grid-container">
+  <div class="item1"id="stxt">Loan Amount
+  <p id="ptxt">RS 3,50,000</p>
+  </div>
+  <div class="item2"id="stxt">Product/Control Program
+  <p id="ptxt">NeoCash Insta</p>
+  </div>
+  <div class="item3" id="stxt">Scheme
+  <p id="ptxt">-</p>
+  </div>  
+  <div class="item4" id="stxt">Constitution Type
+  <p id="ptxt">Pvt.LTD</p>
+  </div>
+  <div class="item5" id="stxt">Industry
+  <p id="ptxt">Advertising</p>
+  </div>
+  <div class="item6" id="stxt">Cust ID Exposure
+  <p id="ptxt">12HDFGJJ</p>
+  </div>
+  
+</div>
+</div>
+</div>
+       
+);
+  }
+}
+
+export default App;
